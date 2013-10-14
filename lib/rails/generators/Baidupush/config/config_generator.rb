581d08d87473bbf5d6c0a8af391b25b85fe60198
zql <hahazhouqunli@gmail.com>
2013-10-14 16:37:20 +0800

    ..

---
+++ lib/rails/generators/Baidupush/config/config_generator.rb
@@ -0,0 +1,16 @@
+# encoding: utf-8
+
+module Baidupush
+  # Your code goes here...
+  module Generators
+    class ConfigGenerator < Rails::Generators::Base
+      desc "Creates a baidu_cloud_push configuration file at config/baidu_push.yml"
+
+      def create_config_file
+        template 'baidu_cloud_push.yml', File.join('config', 'baidu_cloud_push.yml')
+      end
+
+    end
+  end
+end
+
