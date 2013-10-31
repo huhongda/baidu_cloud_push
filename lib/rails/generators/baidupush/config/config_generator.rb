7bcebc8f3a19832304dd87f43531b4341d6b7363
zql <hahazhouqunli@gmail.com>
2013-10-31 16:31:15 +0800

    修复了BUG

--- lib/rails/generators/baidupush/config/config_generator.rb
+++ lib/rails/generators/baidupush/config/config_generator.rb
@@ -4,9 +4,11 @@ module Baidupush
   # Your code goes here...
   module Generators
     class ConfigGenerator < ::Rails::Generators::Base
+      source_root File.expand_path('../templates', __FILE__)
       desc "Creates a baidu_cloud_push configuration file at config/baidu_push.yml"
 
-      def self.create_config_file
+      def generate_config
+        p "Come in!"
         template 'baidu_cloud_push.yml', File.join('config', 'baidu_cloud_push.yml')
       end
 
